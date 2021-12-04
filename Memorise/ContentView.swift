e9c4c6c0e3823ecc1ae40470591e50a382e72148
ThomasR39 <thomasroff390@gmail.com>
2021-12-04 15:42:32 +1300

    Created project

---
+++ Memorise/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Memorise
+//
+//  Created by Thomas Roff on 4/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+.previewInterfaceOrientation(.landscapeLeft)
+    }
+}
---
+++ Memorise/MemoriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriseApp.swift
+//  Memorise
+//
+//  Created by Thomas Roff on 4/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MemoriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
