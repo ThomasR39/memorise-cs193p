d3d82eb585e98f96d3b93b360aacc11f51c56e42
ThomasR39 <thomasroff390@gmail.com>
2021-12-05 20:13:37 +1300

    Established MVVM, view now reflects model

--- Memorise/EmojiMemoryGame.swift
+++ Memorise/EmojiMemoryGame.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-class EmojiMemoryGame {
+class EmojiMemoryGame: ObservableObject {
 
     static let emojis = ["🦜", "🦒", "🐏", "🐓", "🐥", "🦄", "🐁", "🦢", "🦦", "🦩", "🐬", "🐖", "🐇", "🐿", "🦔", "🦙", "🦑", "🐄", "🦏", "🕊"]
 
@@ -17,9 +17,15 @@ class EmojiMemoryGame {
         }
     }
 
-    private var model: MemoryGame<String> = createMemoryGame()
+    @Published private var model: MemoryGame<String> = createMemoryGame()
 
     var cards: Array<MemoryGame<String>.Card> {
-        return model.cards
+        model.cards
+    }
+   
+    // MARK: - Intent(s)
+ 
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card)
     }
 }
