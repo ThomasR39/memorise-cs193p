b13c7f50e4e5d6d519394ef1dc9442f14b2f0fb2
ThomasR39 <thomasroff390@gmail.com>
2021-12-05 15:09:05 +1300

    Prototyping card layout

--- Memorise/ContentView.swift
+++ Memorise/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🦜", "🦒", "🐏", "🐓", "🐥", "🦄", "🐁", "🦢", "🦦", "🦩", "🐬", "🐖", "🐇", "🐿", "🦔", "🦙", "🦑", "🐄", "🦏", "🕊"]
+    
+    @State var emojiCount = 20
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 3)
-            Text("Hello, World!")
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji)
+                    }       .aspectRatio(2/3, contentMode: .fit)
+                }
+            }
+            .foregroundColor(.red)
         }
         .padding(.horizontal)
-        .foregroundColor(.red)
+    }
+}
+
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
+
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                    shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
     }
 }
 
@@ -30,5 +58,3 @@ struct ContentView_Previews: PreviewProvider {
 
     }
 }
-
-
