afe624fd8918f35d8f0e425c0745d5a2e7939666
ThomasR39 <thomasroff390@gmail.com>
2021-12-08 10:54:50 +1300

    Change magic numbers inside view to constants
    Instead of having hard coded values for EmojiMemoryGameView,
    move them into a static constants inside a struct named DrawingConstants.
    Move font size calculation from view to a function for readability.

--- Memorise/EmojiMemoryGameView.swift
+++ Memorise/EmojiMemoryGameView.swift
@@ -33,11 +33,11 @@ struct CardView: View {
     var body: some View {
         GeometryReader{ geometry in
             ZStack {
-                let shape = RoundedRectangle(cornerRadius: 20)
+                let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
                 if card.isFaceUp {
                     shape.fill().foregroundColor(.white)
-                    shape.strokeBorder(lineWidth: 3)
-                    Text(card.content).font(Font.system(size: min(geometry.size.width, geometry.size.height) * 0.8))
+                    shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
+                    Text(card.content).font(font(in: geometry.size))
                 } else if card.isMatched {
                     shape.opacity(0)
                 } else {
@@ -45,6 +45,16 @@ struct CardView: View {
                 }
             }
         }
+    }
+    
+    private func font(in size: CGSize) -> Font {
+        Font.system(size: min(size.width, size.height) * DrawingConstants.fontScale)
+    }
+    
+    private struct DrawingConstants {
+        static let cornerRadius: CGFloat = 20
+        static let lineWidth: CGFloat = 3
+        static let fontScale: CGFloat = 0.8
     }
 }
 
