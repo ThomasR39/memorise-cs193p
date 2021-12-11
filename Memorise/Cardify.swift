07306aec3c94b061d46894d9bd3748ef0e35b9a6
ThomasR39 <thomasroff390@gmail.com>
2021-12-11 20:54:02 +1300

    Create a ViewModifier to "card-ify" a View
    Cardify takes a view and puts it on a card.
    Use View's extended attribute cardify on a View to accomplish this.

---
+++ Memorise/Cardify.swift
@@ -0,0 +1,37 @@
+//
+//  Cardify.swift
+//  Memorise
+//
+//  Created by Thomas Roff on 11/12/21.
+//
+
+import SwiftUI
+
+struct Cardify: ViewModifier {
+    
+    var isFaceUp: Bool
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
+                content
+            } else {
+                shape.fill()
+            }
+        }
+    }
+    
+    private struct DrawingConstants {
+        static let cornerRadius: CGFloat = 10
+        static let lineWidth: CGFloat = 3
+    }
+}
+
+extension View {
+    func cardify(isFaceUp: Bool) -> some View {
+        self.modifier(Cardify(isFaceUp: isFaceUp))
+    }
+}
--- Memorise/EmojiMemoryGameView.swift
+++ Memorise/EmojiMemoryGameView.swift
@@ -31,23 +31,15 @@ struct CardView: View {
     let card: EmojiMemoryGame.Card
     
     var body: some View {
-        GeometryReader{ geometry in
+        GeometryReader { geometry in
             ZStack {
-                let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
-                if card.isFaceUp {
-                    shape.fill().foregroundColor(.white)
-                    shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
-                    Text(card.content).font(font(in: geometry.size))
-                    Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 110-90))
+                    Pie(startAngle: Angle(degrees: 0 - 90), endAngle: Angle(degrees: 110 - 90))
                         .padding(DrawingConstants.timerPadding).opacity(DrawingConstants.timerOpacity)
-                } else if card.isMatched {
-                    shape.opacity(0)
-                } else {
-                    shape.fill()
+                    Text(card.content).font(font(in: geometry.size))
                 }
+            .cardify(isFaceUp: card.isFaceUp)
             }
         }
-    }
     
     private func font(in size: CGSize) -> Font {
         Font.system(size: min(size.width, size.height) * DrawingConstants.fontScale)
