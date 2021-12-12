25269c5224c4f766a858c664db939752a03adf0e
ThomasR39 <thomasroff390@gmail.com>
2021-12-12 14:04:19 +1300

    Use implicit animation to flip card
    Card content should always be on the screen.
    Otherwise, the card will not animate.
    Use opacity to hide and show content
    instead of adding and removing content on the screen.

--- Memorise/EmojiMemoryGameView.swift
+++ Memorise/EmojiMemoryGameView.swift
@@ -34,21 +34,25 @@ struct CardView: View {
         GeometryReader { geometry in
             ZStack {
                     Pie(startAngle: Angle(degrees: 0 - 90), endAngle: Angle(degrees: 110 - 90))
-                        .padding(DrawingConstants.timerPadding).opacity(DrawingConstants.timerOpacity)
-                    Text(card.content).font(font(in: geometry.size))
+                        .padding(DrawingConstants.timerPadding)
+                        .opacity(DrawingConstants.timerOpacity)
+                    Text(card.content)
+                        .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
+                        .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
+                        .font(Font.system(size: DrawingConstants.fontSize))
+                        .scaleEffect(scale(thatFits: geometry.size))
                 }
             .cardify(isFaceUp: card.isFaceUp)
             }
         }
     
-    private func font(in size: CGSize) -> Font {
-        Font.system(size: min(size.width, size.height) * DrawingConstants.fontScale)
+    private func scale(thatFits size: CGSize) -> CGFloat {
+        min(size.width, size.height) / (DrawingConstants.fontSize / DrawingConstants.fontScale)
     }
     
     private struct DrawingConstants {
-        static let cornerRadius: CGFloat = 10
-        static let lineWidth: CGFloat = 3
         static let fontScale: CGFloat = 0.7
+        static let fontSize: CGFloat = 32
         static let timerPadding: CGFloat = 5
         static let timerOpacity: CGFloat = 0.5
     }
